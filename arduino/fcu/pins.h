8341faca849cf58e9c2234ca7508bbce2daf710b
Thorsten Kober <thorsten.kober@softgarden.de>
2020-08-30 21:50:37 +0200

    Defined pins

--- arduino/fcu/pins.h
+++ arduino/fcu/pins.h
@@ -3,4 +3,40 @@
 
 #define ANALOG_PIN(_pin)              (_pin+54)
 
+#define FD_BUTTON_PIN                 22
+
+#define ILS_BUTTON_PIN                24
+
+#define LOC_BUTTON_PIN                26
+
+#define AP_1_BUTTON_PIN               28
+
+#define AP_2_BUTTON_PIN               30
+
+#define A_THR_BUTTON_PIN              32
+
+#define EPED_BUTTON_PIN               34
+
+#define APPR_BUTTON_PIN               36
+
+#define BARO_ROTARY_BUTTON_PIN        38
+#define BARO_ROTARY_ENCODER_PIN_1     40
+#define BARO_ROTARY_ENCODER_PIN_2     42
+
+#define SPD_ROTARY_BUTTON_PIN         44
+#define SPD_ROTARY_ENCODER_PIN_1      46
+#define SPD_ROTARY_ENCODER_PIN_2      48
+
+#define HDG_ROTARY_BUTTON_PIN         23
+#define HDG_ROTARY_ENCODER_PIN_1      25
+#define HDG_ROTARY_ENCODER_PIN_2      27
+
+#define ALT_ROTARY_BUTTON_PIN         29
+#define ALT_ROTARY_ENCODER_PIN_1      31
+#define ALT_ROTARY_ENCODER_PIN_2      33
+
+#define VS_ROTARY_BUTTON_PIN          35
+#define VS_ROTARY_ENCODER_PIN_1       37
+#define VS_ROTARY_ENCODER_PIN_2       39
+
 #endif

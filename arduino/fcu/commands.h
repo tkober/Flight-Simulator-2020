e3092d1b33f65c8477dea63bf073be7f43a7cfdd
Thorsten Kober <thorsten.kober@softgarden.de>
2020-09-17 14:05:52 +0200

    Changed commands to match java enum notation

--- arduino/fcu/commands.h
+++ arduino/fcu/commands.h
@@ -2,40 +2,40 @@
 #define FCU_COMMANDS
 
 
-#define COMMAND_FD_TOGGLE             "fd-toggle"
-#define COMMAND_ILS_TOGGLE            "ils-toggle"
+#define COMMAND_FD_TOGGLE             "FD_TOGGLE"
+#define COMMAND_ILS_TOGGLE            "ILS_TOGGLE"
 
-#define COMMAND_LOC_TOGGLE            "loc-toggle"
+#define COMMAND_LOC_TOGGLE            "LOC_TOGGLE"
 
-#define COMMAND_AP_1_TOGGLE           "ap1-toggle"
-#define COMMAND_AP_2_TOGGLE           "ap2-toggle"
-#define COMMAND_A_THR_TOGGLE          "a_thr-toggle"
+#define COMMAND_AP_1_TOGGLE           "AP1_TOGGLE"
+#define COMMAND_AP_2_TOGGLE           "AP2_TOGGLE"
+#define COMMAND_A_THR_TOGGLE          "A_THR_TOGGLE"
 
-#define COMMAND_EXPED_TOGGLE          "exped-toggle"
-#define COMMAND_APPR_TOGGLE           "appr-toggle"
+#define COMMAND_EXPED_TOGGLE          "EXPED_TOGGLE"
+#define COMMAND_APPR_TOGGLE           "APPR_TOGGLE"
 
-#define COMMAND_BARO_SET_STD          "baro-set-std"
-#define COMMAND_BARO_INCREMENT        "baro-increment"
-#define COMMAND_BARO_DECREMENT        "baro-decrement"
+#define COMMAND_BARO_SET_STD          "BARO_SET_STD"
+#define COMMAND_BARO_INCREMENT        "BARO_INCREMENT"
+#define COMMAND_BARO_DECREMENT        "BARO_DECREMENT"
 
-#define COMMAND_SPD_SELECT            "spd-select"
-#define COMMAND_SPD_MANAGED           "spd-managed"
-#define COMMAND_SPD_INCREMENT         "spd-increment"
-#define COMMAND_SPD_DECREMENT         "spd-decrement"
+#define COMMAND_SPD_SELECT            "SPD_SELECT"
+#define COMMAND_SPD_MANAGED           "SPD_MANAGED"
+#define COMMAND_SPD_INCREMENT         "SPD_INCREMENT"
+#define COMMAND_SPD_DECREMENT         "SPD_DECREMENT"
 
-#define COMMAND_HDG_SELECT            "hdg-select"
-#define COMMAND_HDG_MANAGED           "hdg-managed"
-#define COMMAND_HDG_INCREMENT         "hdg-increment"
-#define COMMAND_HDG_DECREMENT         "hdg-decrement"
+#define COMMAND_HDG_SELECT            "HDG_SELECT"
+#define COMMAND_HDG_MANAGED           "HDG_MANAGED"
+#define COMMAND_HDG_INCREMENT         "HDG_INCREMENT"
+#define COMMAND_HDG_DECREMENT         "HDG_DECREMENT"
 
-#define COMMAND_ALT_SELECT            "alt-select"
-#define COMMAND_ALT_MANAGED           "alt-managed"
-#define COMMAND_ALT_INCREMENT         "alt-increment"
-#define COMMAND_ALT_DECREMENT         "alt-decrement"
+#define COMMAND_ALT_SELECT            "ALT_SELECT"
+#define COMMAND_ALT_MANAGED           "ALT_MANAGED"
+#define COMMAND_ALT_INCREMENT         "ALT_INCREMENT"
+#define COMMAND_ALT_DECREMENT         "ALT_DECREMENT"
 
 
-#define COMMAND_LEVEL_OFF             "level-off"
-#define COMMAND_VS_INCREMENT          "vs-increment"
-#define COMMAND_VS_DECREMENT          "vs-decrement"
+#define COMMAND_LEVEL_OFF             "LEVEL_OFF"
+#define COMMAND_VS_INCREMENT          "VS_INCREMENT"
+#define COMMAND_VS_DECREMENT          "VS_DECREMENT"
 
 #endif

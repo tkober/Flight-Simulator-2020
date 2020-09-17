89317092867f7ef85c167323968647723cf8c606
kober <thorsten.kober@gmail.com>
2020-09-17 17:31:32 +0200

    Implemented SerialDevice

---
+++ java/src/main/java/me/kobair/msfs2020/fcu/lib/hardware/serial/SerialDevice.java
@@ -0,0 +1,87 @@
+package me.kobair.msfs2020.fcu.lib.hardware.serial;
+
+import com.fazecast.jSerialComm.SerialPort;
+import com.fazecast.jSerialComm.SerialPortEvent;
+import com.fazecast.jSerialComm.SerialPortMessageListener;
+import me.kobair.msfs2020.fcu.lib.Command;
+import me.kobair.msfs2020.fcu.lib.hardware.CommandHandler;
+import me.kobair.msfs2020.fcu.lib.hardware.Device;
+
+public class SerialDevice implements Device, SerialPortMessageListener {
+
+    private SerialPort serialPort;
+    private CommandHandler commandHandler;
+    private String messageDelimiter = NEW_LINE_MESSAGE_DELIMITER;
+
+    protected SerialPort getSerialPort() {
+        return this.serialPort;
+    }
+
+    @Override
+    public String getMessageDelimiterString() {
+        return this.messageDelimiter;
+    }
+
+    @Override
+    public void setMessageDelimiterString(String messageDelimiter) {
+        this.messageDelimiter = messageDelimiter;
+    }
+
+    @Override
+    public boolean isConnected() {
+        return this.serialPort.isOpen();
+    }
+
+    @Override
+    public String getName() {
+        return this.serialPort.getPortDescription();
+    }
+
+    @Override
+    public void setCommandHandler(CommandHandler commandHandler) {
+        this.commandHandler = commandHandler;
+    }
+
+    @Override
+    public void purgeCommandHandler() {
+        this.commandHandler = null;
+    }
+
+    @Override
+    public void startReceivingMessages() {
+        this.serialPort.addDataListener(this);
+    }
+
+    @Override
+    public void stopReceivingMessages() {
+        this.serialPort.removeDataListener();
+    }
+
+    @Override
+    public int getListeningEvents() {
+        return SerialPort.LISTENING_EVENT_DATA_RECEIVED;
+    }
+
+    @Override
+    public byte[] getMessageDelimiter() {
+        return this.messageDelimiter.getBytes();
+    }
+
+    @Override
+    public boolean delimiterIndicatesEndOfMessage() {
+        return true;
+    }
+
+    @Override
+    public void serialEvent(SerialPortEvent event)
+    {
+        final byte[] data = event.getReceivedData();
+        final String text  = new String(data);
+        final String commandString = text.trim();
+        final Command command = Command.valueOf(commandString);
+
+        if (this.commandHandler != null) {
+            this.commandHandler.handleCommand(command);
+        }
+    }
+}

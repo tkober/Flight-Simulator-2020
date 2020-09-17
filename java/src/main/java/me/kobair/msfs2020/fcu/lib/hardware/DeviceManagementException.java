4400ac1d6387446a065892f25e51facfd663e04e
kober <thorsten.kober@gmail.com>
2020-09-17 17:33:45 +0200

    Exception subclasses are now public and static

--- java/src/main/java/me/kobair/msfs2020/fcu/lib/hardware/DeviceManagementException.java
+++ java/src/main/java/me/kobair/msfs2020/fcu/lib/hardware/DeviceManagementException.java
@@ -6,14 +6,14 @@ public abstract class DeviceManagementException extends Exception {
         super(message);
     }
 
-    class DuplicateDeviceException extends DeviceManagementException {
+    public static class DuplicateDeviceException extends DeviceManagementException {
 
         public DuplicateDeviceException(final String deviceName) {
             super(String.format("A device with the name '%s' is already being managed.", deviceName));
         }
     }
 
-    class IllegalDeviceException extends DeviceManagementException {
+    public static class IllegalDeviceException extends DeviceManagementException {
 
         public IllegalDeviceException(final Device device) {
             super(String.format("The device %s is not managed by this DeviceManager.", device.toString()));
